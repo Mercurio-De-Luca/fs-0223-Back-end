d9794ac85b19c88840a038e8fa047b74cee83148
Mercurio De Luca <mirkodeluca413@gmail.com>
2023-06-26 19:16:58 +0200

    esercizio_1

--- EpicodeBackendCourse1/EpicodeBackendCourse1/src/it/epicode/be/Helloworld.java
+++ EpicodeBackendCourse1/EpicodeBackendCourse1/src/it/epicode/be/Helloworld.java
@@ -2,13 +2,36 @@ package it.epicode.be;
 
 public class Helloworld {
 
-public static void main (String[] args)
-		{
-		// Stampa a console  la frase
-		System.out.println("This is my first Epicode Java Project!");
+public static void main (String[] args){
+	
+		String[] persone = {"Mirko","Matteo","Francesca","Giovanna","Leo"};
+	
+		System.out.println(moltiplica(2,5));
+		
+		System.out.println(concatena("ciao", 23));
+		
+		
+
 		}
 
-
+public static int moltiplica(int x, int y) {
+	return x * y;
+}
+public static String concatena(String x, int y) {
+	return x + y;
+}
+public static String[] inserisciInArray(String [] x, String y) {
+	String[] arr = new String[6];
+	arr[0] = x[0];
+	arr[1] = x[1];
+	arr[2] = x[2];
+	arr[3] = y;
+	arr[4] = x[4];
+	arr[5] = x[5];
+	
+	return arr;
+}
+	
 }
 
 
